1f460194057a724b6bb148417e3a35b0031de258
Vehniäinen Johanna R <xjoxjox@dat-8.cs.helsinki.fi>
2016-05-18 14:53:01 +0300

    kirjautumistoiminnon aloitus

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by username: params[:username]
+    if user.nil?
+      redirect_to :back, notice: "Käyttäjää #{params[:username]} ei löydy!"
+    else
+      session[:user_id] = user.id if not user.nil?
+      redirect_to user, notice: "Tervetuloa #{params[:username]}!"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to :signin
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   resources :users
+
+  root 'users#index'
+
+  get 'signin', to: 'sessions#new'
+  get 'signup', to: 'users#new'
+  delete 'signout', to: 'sessions#destroy'
+
+  resource :session, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
