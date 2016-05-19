5a7b376e270700ca59be9a34b31b191864fb9af3
kjh <kalle.hiljanen@helsinki.fi>
2016-05-19 23:31:57 +0300

    user kenttien nimet + virheilmoitukset suomeksi

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :username, uniqueness: true, length:{minimum: 2, maximum: 30, message:
-      "käyttäjätunnuksen tulee olla 2-30 merkkiä pitkä, eikä se saa olla varattu"}
+      "tulee olla 2-30 merkkiä pitkä, eikä se saa olla varattu"}
   validates :password, :format => {:with => /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{4,}\z/, message:
-      "salasanan tulee olla vähintään 4 merkkiä pitkä ja sisältää yhden numeron ja yhden ison kirjaimen"}
+      "tulee olla vähintään 4 merkkiä pitkä ja sisältää yhden numeron ja yhden ison kirjaimen"}
 end
