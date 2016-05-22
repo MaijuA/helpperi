93e22df9a8eaae1b822e2f4b0ad3268a6835c4ad
kjh <kalle.hiljanen@helsinki.fi>
2016-05-22 14:43:18 +0300

    smtp asetukset poistettu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,18 +91,18 @@ Rails.application.configure do
   #    :domain         => 'heroku.com',
   #    :enable_starttls_auto => true
   #}
-  config.action_mailer.default_url_options = { :host => 'helpperi.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "gmail.com",
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      password: ENV["GMAIL_PASSWORD"]
-  }
+  #config.action_mailer.default_url_options = { :host => 'helpperi.herokuapp.com' }
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.default :charset => "utf-8"
+  #config.action_mailer.smtp_settings = {
+  #    address: "smtp.gmail.com",
+  #    port: 587,
+  #    domain: "gmail.com",
+  #    authentication: "plain",
+  #    enable_starttls_auto: true,
+  #    user_name: ENV["GMAIL_USERNAME"],
+  #    password: ENV["GMAIL_PASSWORD"]
+  #}
 end
