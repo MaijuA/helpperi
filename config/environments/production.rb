d1cd8d8bd4e62ba674d4a2c2731f7ffb895e936c
kjh <kalle.hiljanen@helsinki.fi>
2016-05-23 10:45:06 +0300

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,15 +83,15 @@ Rails.application.configure do
   config.assets.initialize_on_precompile = false
   config.action_mailer.default_url_options = { :host => 'helpperi.herokuapp.com' }
 
-  ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
-  }
+  #ActionMailer::Base.smtp_settings = {
+  #    :address        => 'smtp.sendgrid.net',
+  #    :port           => '587',
+  #    :authentication => :plain,
+  #    :user_name      => ENV['SENDGRID_USERNAME'],
+  #    :password       => ENV['SENDGRID_PASSWORD'],
+  #    :domain         => 'heroku.com',
+  #    :enable_starttls_auto => true
+  #}
 
   #ActionMailer::Base.smtp_settings = {
   #    :address        => "smtp.sendgrid.net",
