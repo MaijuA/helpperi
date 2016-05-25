521a0df39ea728bc2afe7e93132f424487c92e73
Vehniäinen Johanna R <xjoxjox@dat-8.cs.helsinki.fi>
2016-05-25 12:09:17 +0300

    travis korjausta

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   add_filter 'app/secrets'
 end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
