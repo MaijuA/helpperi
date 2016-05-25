c75ccc4b173b245893787cc2cd52b4f712f37517
kjh <kalle.hiljanen@helsinki.fi>
2016-05-25 13:36:45 +0300

    hetun validointi

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include CustomValidations
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable,
@@ -10,7 +12,9 @@ class User < ActiveRecord::Base
             }, if: :password_required?
   validates :first_name, :last_name, :personal_code, :phone_number, :address, :zip_code, :city, presence: true
   validates :phone_number, phone: { possible: true}
-  #def is_req?
-  #  !persisted? || !password.nil? || !password_confirmation.nil?
-  #end
+  validate :hetu
+
+  def hetu
+    errors.add(:personal_code, "ei ole validi") unless hetu_valid?(personal_code)
+  end
 end
