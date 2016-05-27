d5039fadba79b335262703024009c5b8c063f4f8
kjh <kalle.hiljanen@helsinki.fi>
2016-05-27 21:09:55 +0300

    migraatiot

--- db/migrate/20160520075301_add_devise_to_users.rb
+++ db/migrate/20160520075301_add_devise_to_users.rb
@@ -28,7 +28,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
       ## Lockable
       t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       t.string   :unlock_token # Only if unlock strategy is :email or :both
-      t.datetime :locked_at
+      #t.datetime :locked_at # lock lisätty
 
 
       # Uncomment below if timestamps were not included in your original model.
--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,8 @@ ActiveRecord::Schema.define(version: 20160527104222) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",        default: 0,  null: false
     t.string   "unlock_token"
-    #t.datetime "locked_at"
+    t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "confirmation_token"
