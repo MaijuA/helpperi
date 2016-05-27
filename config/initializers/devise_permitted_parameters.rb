10b0e4d4ff081d1820d1ce6b91463bb8117af877
Eetu Mattila <eetu.mattila@cs.helsinki.fi>
2016-05-27 15:45:30 +0300

    Kuvan uploadaus profiiliin

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,7 +8,7 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    parameters = [:first_name, :last_name, :personal_code, :passport_number, :phone_number, :address, :zip_code, :city, :description]
+    parameters = [:first_name, :last_name, :personal_code, :passport_number, :phone_number, :address, :zip_code, :city, :description, :image, :remove_image, :image_cache]
     devise_parameter_sanitizer.permit(:sign_up, keys: parameters)
     devise_parameter_sanitizer.permit(:account_update, keys: parameters)
   end
