0783267ac5d602dcd7afd062f5d8d2938f6c7f02
kjh <kalle.hiljanen@helsinki.fi>
2016-05-28 12:47:22 +0300

    devise testejä lisätty

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -5,8 +5,9 @@ Warden.test_mode!
 
 describe 'User sign in' do
 
+  let(:user) { FactoryGirl.create(:user) }
+
   it 'user can sign in' do
-    user = FactoryGirl.create(:user)
 
     visit new_user_session_path
 
@@ -17,6 +18,27 @@ describe 'User sign in' do
     click_button('Kirjaudu')
 
     expect(page).to have_content 'Sisäänkirjautuminen onnistui.'
+
+  end
+
+  it 'is lockable' do
+
+    user.password += 'Not-My-Password'
+
+    visit new_user_session_path
+
+    10.times do
+
+      fill_in('user_email', with: user.email)
+
+      fill_in('user_password', with: user.password)
+
+      click_button('Kirjaudu')
+
+    end
+
+    expect(page).to have_content 'Tunnuksesi on lukittu.'
+
   end
 
 end
