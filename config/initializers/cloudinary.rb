469698d7b82f9e595de2b18e2f8ca1006cb11d1d
kjh <kalle.hiljanen@helsinki.fi>
2016-05-30 12:56:05 +0300

    lomakkeet päivitetty ja travis deploy muokattu

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -3,4 +3,5 @@ Cloudinary.config do |config|
   config.api_key = ENV['CLOUDINARY_API_KEY']
   config.api_secret = ENV['CLOUDINARY_SECRET_KEY']
   config.cdn_subdomain = true
+  config.secure = true
 end
