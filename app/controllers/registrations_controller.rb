b986a7ad68221e758c2d189815b74b1dc51138b1
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-06-01 11:45:36 +0300

    ilmoituksen poisto

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def destroy
+    current_user.posts.each do |p|
+      p.update_attribute(:deleted, true)
+    end
     resource.soft_delete
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     set_flash_message :notice, :destroyed if is_flashing_format?
