d0e4e6d40946b46468c71ab3ffc4339ee07b18ad
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-06-03 14:46:45 +0300

    oauth toimintoja

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -21,8 +21,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "#{provider}") if is_navigational_format?
     else
-      session["devise.provider_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
+      redirect_to root_path, notice: 'Kirjautuminen/Rekisteröityminen ei onnistunut.'
     end
   end
 
