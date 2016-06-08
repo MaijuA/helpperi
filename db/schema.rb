5769a3c983c739f43c614a1b94111b3394085c55
kjh <kalle.hiljanen@helsinki.fi>
2016-06-08 21:45:06 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606081230) do
+ActiveRecord::Schema.define(version: 20160607104625) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,25 @@ ActiveRecord::Schema.define(version: 20160606081230) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "post_categories", force: :cascade do |t|
     t.integer  "post_id"
@@ -76,9 +95,9 @@ ActiveRecord::Schema.define(version: 20160606081230) do
     t.boolean  "passport_number"
     t.datetime "deleted_at"
     t.string   "image"
+    t.string   "language"
     t.string   "provider"
     t.string   "uid"
-    t.string   "language"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
