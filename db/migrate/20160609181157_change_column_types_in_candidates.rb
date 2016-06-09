d2fcc7731072c8f24653c454777c59dd2c61fb6a
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-06-09 21:55:44 +0300

    herokun korjausta

--- db/migrate/20160609181157_change_column_types_in_candidates.rb
+++ db/migrate/20160609181157_change_column_types_in_candidates.rb
@@ -1,6 +1,9 @@
 class ChangeColumnTypesInCandidates < ActiveRecord::Migration
   def change
-    change_column :candidates, :post_id, :integer, 'integer USING CAST(post_id AS integer)'
-    change_column :candidates, :user_id, :integer, 'integer USING CAST(user_id AS integer)'
+    remove_column :candidates, :post_id
+    remove_column :candidates, :user_id
+
+    add_column :candidates, :post_id, :integer
+    add_column :candidates, :user_id, :integer
   end
 end
