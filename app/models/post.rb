71bb7c4e4fc0c1ec029c36deb0a7474876e6268d
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-06-09 11:19:42 +0300

    kiinnostuneet painikkeet kuntoon

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ActiveRecord::Base
   has_many :categories, -> {distinct}, through: :post_categories
   has_many :interesteds, -> { where denyed: false }
   has_many :helpers, -> {distinct}, through: :interesteds, source: :user
-  has_many :denyed_helpers, -> { where denyed: true }, through: :interesteds, source: :user
 
   mount_uploader :image, ImageUploader
 
@@ -44,6 +43,10 @@ class Post < ActiveRecord::Base
 
   def image_is_set?
     self.image.file != nil
+  end
+
+  def denyed_helpers
+    User.joins(:interesteds).where(:interesteds => {denyed:true})
   end
 
   scope :active, -> { where('deleted IS ? AND doer_id IS NULL', false) }
