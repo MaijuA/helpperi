f22a7935d254be7825f0f640979d189df6541ad8
kjh <kalle.hiljanen@helsinki.fi>
2016-06-12 12:41:52 +0300

    migraatioita muokattu ja poistettu

--- db/migrate/20160612092456_change_messages_timestamps.rb
+++
@@ -1,6 +0,0 @@
-class ChangeMessagesTimestamps < ActiveRecord::Migration
-  def change
-    change_column :messages, :created_at, :datetime, null: false
-    change_column :messages, :updated_at, :datetime, null: false
-  end
-end
