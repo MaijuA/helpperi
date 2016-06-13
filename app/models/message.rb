884714fe2097aa2239865ab8fd487f142736e603
kjh <kalle.hiljanen@helsinki.fi>
2016-06-13 10:18:31 +0300

    viestit muokattu

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,10 +2,13 @@ class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
 
-  #validates_presence_of :body, :conversation_id, :user_id
+  scope :unread, -> do
+    where('read = ?', false)
+  end
+
+  validates_presence_of :body, :conversation_id, :user_id
 
   def message_time
-    # I18n.l created_at
-    created_at.strftime("%m/%d/%y at %l:%M %p")
+    I18n.l created_at, format: :short
   end
 end
