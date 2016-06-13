2417728d60c44908d29797b5b614830b60c1a1b5
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-06-13 16:09:13 +0300

    bugien korjaus

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,23 @@
 
 ActiveRecord::Schema.define(version: 20160613100756) do
 
+  create_table "activities", force: :cascade do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
+
   create_table "candidates", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -32,21 +49,21 @@ ActiveRecord::Schema.define(version: 20160613100756) do
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
     t.integer  "recipient_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "messages", force: :cascade do |t|
     t.text     "body"
-    t.integer  "conversations_id"
+    t.integer  "conversation_id"
     t.integer  "user_id"
-    t.boolean  "read",             default: false
+    t.boolean  "read",            default: false
     t.integer  "post_id"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "messages", ["conversations_id"], name: "index_messages_on_conversations_id"
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
   add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "post_categories", force: :cascade do |t|
@@ -73,6 +90,16 @@ ActiveRecord::Schema.define(version: 20160613100756) do
     t.string   "post_type"
     t.integer  "doer_id"
   end
+
+  create_table "read_marks", force: :cascade do |t|
+    t.integer  "readable_id"
+    t.string   "readable_type", null: false
+    t.integer  "reader_id"
+    t.string   "reader_type",   null: false
+    t.datetime "timestamp"
+  end
+
+  add_index "read_marks", ["reader_id", "reader_type", "readable_type", "readable_id"], name: "read_marks_reader_readable_index"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -105,9 +132,9 @@ ActiveRecord::Schema.define(version: 20160613100756) do
     t.boolean  "passport_number"
     t.datetime "deleted_at"
     t.string   "image"
-    t.string   "language"
     t.string   "provider"
     t.string   "uid"
+    t.string   "language"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
