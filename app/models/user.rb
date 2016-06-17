ee5bfd1ef8281ee70124e19563ff7e1d98b5a23a
Eetu Mattila <eetu.mattila@cs.helsinki.fi>
2016-06-17 15:07:37 +0300

    Arvostelujen liitos user modeliin korjattu

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,8 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :candidates, -> { where denied:false }, dependent: :destroy
   has_many :tasks, through: :candidates, source: :post
-  has_many :ratings
+  has_many :ratings, :class_name => "Rating", :foreign_key => 'reviewed_id', :dependent => :destroy
+  has_many :reviews, :class_name => "Rating", :foreign_key => 'reviewer_id', :dependent => :destroy
 
   scope :active, -> { where deleted_at:false }
   scope :deleted, -> { where deleted_at:true }
