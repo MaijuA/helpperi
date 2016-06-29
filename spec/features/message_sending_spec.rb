b3e7a613ca299514d34fdf96b41961f209e686a7
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-06-29 19:03:39 +0300

    testejä

--- spec/features/message_sending_spec.rb
+++ spec/features/message_sending_spec.rb
@@ -6,16 +6,15 @@ describe 'Messages' do
   let!(:user4) { FactoryGirl.create :user4 }
 
   it 'should send message' do
-    c = Conversation.where(sender_id:user.id).count
     login_as(user)
     visit user_path(user3)
 
     click_link 'Lähetä viesti'
-    find('body').set('heipähei')
+    fill_in('message_body', :with =>'heipähei')
 
     click_button 'Lähetä viesti'
 
-    expect(Conversation.where(sender_id:user.id).count).to eq(c+1)
+    expect(Conversation.where(sender_id:user.id).count).to eq(1)
   end
 
   it 'should redirect to root page if user not part of conversation' do
@@ -23,7 +22,7 @@ describe 'Messages' do
     visit user_path(user3)
 
     click_link 'Lähetä viesti'
-    find('body').set('heipähei')
+    fill_in('message_body', :with =>'heipähei')
 
     click_button 'Lähetä viesti'
 
@@ -32,6 +31,33 @@ describe 'Messages' do
     visit conversation_messages_path(Conversation.where(sender_id:user.id).first)
 
     expect(page).to have_content('Hae')
+  end
+
+  it 'should send several messages and mark messages as read for user' do
+    login_as(user)
+    visit user_path(user4)
+
+    click_link 'Lähetä viesti'
+
+    (1..14).each do |i|
+      fill_in('message_body', :with =>'heipähei')
+      click_button 'Lähetä viesti'
+    end
+
+    expect(page).to have_content '10 uusinta'
+    expect(Conversation.where(sender_id: user.id).where(recipient_id:user3.id).user_messages(user.id).unread_by(user).count).to eq(0)
+  end
+
+  it 'should not send empty message' do
+    login_as(user)
+    visit user_path(user4)
+
+    click_link 'Lähetä viesti'
+
+    fill_in('message_body', :with =>'')
+    click_button 'Lähetä viesti'
+
+    expect(page).to have_content 'Viestiä ei voitu lähettää'
   end
 end
 
